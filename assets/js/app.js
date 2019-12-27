9abd376df9c4a8fe7d099f15bfb68e27bdb4b0b1
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2019-12-26 23:31:18 -0500

    Targeted DIVS using jQuery library

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+var targetNumber=Math.floor(Math.random() * 100); 
+
+var targetNumberDiv = $('.target-number'); 
+var userScoreDiv = $('.user-score');
+
+
+
+var azuriteButton =  $('button.azurite'); 
+var flouriteButton = $('button.flourite'); 
+var quartzButton = $('button.quartz'); 
+var topazButton = $('button.topaz'); 
+
+
+// azuriteButton.on("click", () => {
+//     alert('You have clicked something ')
+// });
+
+
+
