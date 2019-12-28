360bddf0e853931c63decc13dbfe160d042171d3
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2019-12-27 22:19:50 -0500

    Initiated functions to add values of crystals

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,26 @@
-var targetNumber=Math.floor(Math.random() * 100); 
+var targetNumber = Math.floor(Math.random() * 100);
 
-var targetNumberDiv = $('.target-number'); 
-var userScoreDiv = $('.user-score');
-
-
-
-var azuriteButton =  $('button.azurite'); 
-var flouriteButton = $('button.flourite'); 
-var quartzButton = $('button.quartz'); 
-var topazButton = $('button.topaz'); 
+var $targetNumberDiv = $('.target-number');
+var $userScoreDiv = $('.user-score');
 
 
+var $azuriteButton = $('.azurite-click');
+$azuriteButton.on('click', () => {
+    alert('You clicked something Azurite related.')
+});
+
+
+var $flouriteButton = $('.flourite-click');
+var $quartzButton = $('.quartz-click');
+var $topazButton = $('.topaz-click');
 // azuriteButton.on("click", () => {
 //     alert('You have clicked something ')
 // });
 
+event.preventDefault();
 
 
+
+var addItUp = () => {
+        
+}
