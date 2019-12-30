0672187bdbe505b82225eded3e47ad979de089a1
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2019-12-30 12:58:55 -0500

    Added function to randomly assign vals to each button on pageload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,37 @@
-var targetNumber = Math.floor(Math.random() * 100);
 
+
+var targetNumber = Math.floor(Math.random() * 100);
 var $targetNumberDiv = $('.target-number');
+$targetNumberDiv.text(targetNumber); 
+
+
 var $userScoreDiv = $('.user-score');
 
-
 var $azuriteButton = $('.azurite-click');
-$azuriteButton.on('click', () => {
-    alert('You clicked something Azurite related.')
-});
-
-
 var $flouriteButton = $('.flourite-click');
 var $quartzButton = $('.quartz-click');
 var $topazButton = $('.topaz-click');
-// azuriteButton.on("click", () => {
-//     alert('You have clicked something ')
-// });
 
-event.preventDefault();
+var randValOne = Math.floor(Math.random() * 25); 
+var randValTwo = Math.floor(Math.random() * 25); 
+var randValThree = Math.floor(Math.random() * 25); 
+
+var buttonVals = [1, randValOne, randValTwo, randValThree];
+randButtnVal = Math.floor(Math.random()*4); 
 
 
 
-var addItUp = () => {
-        
-}
+console.log(buttonVals); 
+
+
+
+var assignVals = () => {
+$azuriteButton.attr('value',buttonVals[randButtnVal]); 
+$flouriteButton.attr('value',buttonVals[randButtnVal]);
+$quartzButton.attr('value',buttonVals[randButtnVal]);
+$topazButton.attr('value',buttonVals[randButtnVal]);
+}; 
+
+assignVals(); 
+
+console.log($azuriteButton.attr('value')); 
