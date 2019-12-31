95bd6f4401eb4680b0f91fc9865c7c5a65eafae4
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2019-12-31 01:04:35 -0500

    Added more possibe values to array and userScores to fight similar button values

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var targetNumber = Math.floor(Math.random() * 100) + 25;
+var targetNumber = Math.floor(Math.random() * 50) + 100;
 var $targetNumberDiv = $('.target-number');
 $targetNumberDiv.text(targetNumber);
 
@@ -10,38 +10,71 @@ var $flouriteButton = $('.flourite-click');
 var $quartzButton = $('.quartz-click');
 var $topazButton = $('.topaz-click');
 
-var randValOne = Math.floor(Math.random() * 25);
-var randValTwo = Math.floor(Math.random() * 25);
-var randValThree = Math.floor(Math.random() * 25);
+var randValOne = Math.floor(Math.random() * 25) + 1;
+var randValTwo = Math.floor(Math.random() * 25) + 1;
+var randValThree = Math.floor(Math.random() * 25) + 1;
+var randValFour = Math.floor(Math.random() * 25) + 1;
+var randValFive = Math.floor(Math.random() * 25) + 1;
+var randValSix = Math.floor(Math.random() * 25) + 1;
+var randValSeven = Math.floor(Math.random() * 25) + 1;
+var randValEight = Math.floor(Math.random() * 25) + 1;
+var randValNine = Math.floor(Math.random() * 25) + 1;
+var randValTen = Math.floor(Math.random() * 25) + 1;
+var randValEleven = Math.floor(Math.random() * 25) + 1;
+var randValTwelve = Math.floor(Math.random() * 25) + 1;
+var randValThirteen = Math.floor(Math.random() * 25) + 1;
+var randValFourteen = Math.floor(Math.random() * 25) + 1;
+var randValFifteen = Math.floor(Math.random() * 25) + 1;
+var randValSixteen = Math.floor(Math.random() * 25) + 1;
+var randValSeventeen = Math.floor(Math.random() * 25) + 1;
+var randValEighteen = Math.floor(Math.random() * 25) + 1;
+var randValNineteen = Math.floor(Math.random() * 25) + 1;
+var randValTwenty = Math.floor(Math.random() * 25) + 1;
+var randValTwentyOne = Math.floor(Math.random() * 25) + 1;
+var randValTwentyTwo = Math.floor(Math.random() * 25) + 1;
+var randValTwentyThree = Math.floor(Math.random() * 25) + 1;
+var randValTwentyFour = Math.floor(Math.random() * 25) + 1;
+var randValTwentyFive = Math.floor(Math.random() * 25) + 1;
 
-var buttonVals = [1, randValOne, randValTwo, randValThree];
-randButtnVal = Math.floor(Math.random() * 4);
+
+var buttonVals = [1, randValOne, randValTwo, randValThree, randValFour, randValFive, randValSix, randValSeven, randValEight, randValNine, randValTen, randValEleven, randValTwelve, randValThirteen, randValFourteen, randValFifteen, randValSixteen, randValSeventeen, randValEighteen, randValNineteen, randValTwenty, randValTwentyOne, randValTwentyTwo, randValTwentyThree, randValTwentyFour, randValTwentyFive];
+
+console.log(buttonVals); 
+
+var randButtnVal1 = Math.floor(Math.random() * 25);
+var randButtnVal2 = Math.floor(Math.random() * 25);
+var randButtnVal3 = Math.floor(Math.random() * 25);
+var randButtnVal4 = Math.floor(Math.random() * 25);
+
+
+
+// console.log(`1: ${randButtnVal1} \n 2: ${randButtnVal2} \n 3: ${randButtnVal3} \n 4: ${randButtnVal4}`);
 
 var assignVals = () => {
-    $azuriteButton.attr('value', buttonVals[randButtnVal]);
-    $flouriteButton.attr('value', buttonVals[randButtnVal]);
-    $quartzButton.attr('value', buttonVals[randButtnVal]);
-    $topazButton.attr('value', buttonVals[randButtnVal]);
+    $azuriteButton.attr('value', buttonVals[randButtnVal1]);
+    $flouriteButton.attr('value', buttonVals[randButtnVal2]);
+    $quartzButton.attr('value', buttonVals[randButtnVal3]);
+    $topazButton.attr('value', buttonVals[randButtnVal4]);
 };
 
 assignVals();
 
-    
-var addAzurite = () => {
+console.log($azuriteButton.attr('value'));
+console.log($flouriteButton.attr('value'));
+console.log($quartzButton.attr('value'));
+console.log($topazButton.attr('value'));
+
+
+
+
+$azuriteButton.on('click', () => {
     console.log($azuriteButton.attr('value'));
     userScore += parseFloat($azuriteButton.attr('value'));
     console.log(userScore);
     $userScoreDiv.text(userScore);
-}; 
+});
 
-var addFlourite = () => {    
-    console.log($flouriteButton.attr('value'));
+$flouriteButton.on('click', () => {
     userScore += parseFloat($flouriteButton.attr('value'));
-    console.log(userScore);
-    $userScoreDiv.text(userScore)
-}; 
-
-
-$azuriteButton.on('click', addAzurite); 
-
-
+    $userScoreDiv.text(userScore);
+});
