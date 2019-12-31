ff06a975c239b6f7445a39f7bbd77802d1a3a5d5
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2019-12-30 23:51:36 -0500

    Fixed issue regarding concat userscore vs adding values

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,8 @@
-
-
-var targetNumber = Math.floor(Math.random() * 100);
+var targetNumber = Math.floor(Math.random() * 100) + 25;
 var $targetNumberDiv = $('.target-number');
-$targetNumberDiv.text(targetNumber); 
+$targetNumberDiv.text(targetNumber);
 
-
+var userScore = 0;
 var $userScoreDiv = $('.user-score');
 
 var $azuriteButton = $('.azurite-click');
@@ -12,26 +10,28 @@ var $flouriteButton = $('.flourite-click');
 var $quartzButton = $('.quartz-click');
 var $topazButton = $('.topaz-click');
 
-var randValOne = Math.floor(Math.random() * 25); 
-var randValTwo = Math.floor(Math.random() * 25); 
-var randValThree = Math.floor(Math.random() * 25); 
+var randValOne = Math.floor(Math.random() * 25);
+var randValTwo = Math.floor(Math.random() * 25);
+var randValThree = Math.floor(Math.random() * 25);
 
 var buttonVals = [1, randValOne, randValTwo, randValThree];
-randButtnVal = Math.floor(Math.random()*4); 
-
-
-
-console.log(buttonVals); 
-
-
+randButtnVal = Math.floor(Math.random() * 4);
 
 var assignVals = () => {
-$azuriteButton.attr('value',buttonVals[randButtnVal]); 
-$flouriteButton.attr('value',buttonVals[randButtnVal]);
-$quartzButton.attr('value',buttonVals[randButtnVal]);
-$topazButton.attr('value',buttonVals[randButtnVal]);
-}; 
+    $azuriteButton.attr('value', buttonVals[randButtnVal]);
+    $flouriteButton.attr('value', buttonVals[randButtnVal]);
+    $quartzButton.attr('value', buttonVals[randButtnVal]);
+    $topazButton.attr('value', buttonVals[randButtnVal]);
+};
 
-assignVals(); 
+assignVals();
 
+$azuriteButton.on('click', () => {
+    console.log(userScore)
+    console.log($azuriteButton.attr('value'));
+    userScore += parseFloat($azuriteButton.attr('value'));
+    console.log(userScore);
+    $userScoreDiv.text(userScore);
+    //    console.log(userScore)
+});
 console.log($azuriteButton.attr('value')); 
